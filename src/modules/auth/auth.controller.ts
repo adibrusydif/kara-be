179189e1ce21d424ad4064277484f8f64f23abb9
Adib Rusydi <adibrusydif@gmail.com>
2024-05-20 19:36:52 +0200

    revert local

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,14 +2,23 @@ import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { LoginDto } from './auth.dto';
+import { UserService } from '../user/user.service';
+import { CreateUserDto } from '../user/user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+    ) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() loginDto: LoginDto) {
     return this.authService.signIn(loginDto);
+  }
+  @Post('register')
+  signUp(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
 }
